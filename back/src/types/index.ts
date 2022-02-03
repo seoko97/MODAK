5320b62c177edb9ddb8d6fc02c3a2f47019581f4
seokho10007 <sukho1007@naver.com>
2022-02-04 00:21:15 +0900

    Feat. Axios Respone Data DTO 생성

---
+++ back/src/types/index.ts
@@ -0,0 +1,16 @@
+export interface IAxiosSchduleDTO {
+	facltNm: string; // 야영장
+	lineIntro: string; // 한줄소개
+	intro: string; // 소개
+	addr1: string; // 주소
+	mapX: number; // 경도
+	mapY: number; // 위도
+	tel: string; // 전화
+	resveUrl: string; // 예약페이지
+	animalCmgCl: string; // 애완견 가능 여부
+	induty: string; // 업종
+	themaEnvrnCl: string; // 테마환경
+	sbrsCl: string; // 부대시설
+	eqpmnLendCl: string; // 장비 대여 여부
+	lctCl: string; // 입지
+}
