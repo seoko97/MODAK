1979df532b87d8cc8f11af3e8e8ae8be39b9a960
seokho10007 <sukho1007@naver.com>
2022-02-04 00:19:44 +0900

    Feat. Campsite Parsing Module 생성

---
+++ back/src/utils/dataParser.ts
@@ -0,0 +1,39 @@
+import { IAxiosSchduleDTO } from '@src/types';
+import { ICampsiteDTO } from '@src/types/Campsite';
+import { splitStr } from './spiltStr';
+
+export const getCampData = (camp: IAxiosSchduleDTO): ICampsiteDTO => {
+	const {
+		facltNm,
+		lineIntro,
+		intro,
+		induty,
+		addr1,
+		mapX,
+		mapY,
+		tel,
+		resveUrl,
+		themaEnvrnCl,
+		sbrsCl,
+		animalCmgCl,
+		eqpmnLendCl,
+		lctCl,
+	}: IAxiosSchduleDTO = camp;
+
+	return {
+		name: facltNm.trim() || '',
+		address: addr1 || '',
+		lineIntro: lineIntro || '',
+		intro: intro || '',
+		x: Number(mapX) || 0,
+		y: Number(mapY) || 0,
+		tel: tel || '',
+		reservationUrl: resveUrl || '',
+		animal: animalCmgCl || '불가능',
+		category: splitStr(induty),
+		thema: splitStr(themaEnvrnCl),
+		amenities: splitStr(sbrsCl),
+		rental: splitStr(eqpmnLendCl),
+		environment: splitStr(lctCl),
+	};
+};
