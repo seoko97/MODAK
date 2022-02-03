c07d3c1232d843815fed0f27218a257aaec9b0df
seokho10007 <sukho1007@naver.com>
2022-02-03 10:42:46 +0900

    Feat. NavItem 분리

--- front/src/components/UI/molecules/Nav/index.tsx
+++ front/src/components/UI/molecules/Nav/index.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import NavItem from "./NavItem";
 
 const StyledNav = styled.ul`
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 12px;
+  & > li {
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 600;
+    & > *:first-of-type {
+      margin-right: 5px;
+    }
+  }
 `;
 
 const Nav = () => {
   return (
     <>
       <StyledNav>
-        <li>
-          <span>CampSite</span>
-        </li>
-        <li>
-          <span>CampSite</span>
-        </li>
+        <NavItem text="CampSite" href="/campsite" />
+        <NavItem text="CampSite" href="/campsite" />
       </StyledNav>
     </>
   );
