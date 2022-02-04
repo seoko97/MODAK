ff11e301a402d07a069b0dcae5ddf8d66847df34
Jisung <jasdepw@gmail.com>
2022-02-05 00:04:58 +0900

    Feat. passport FacebookStrategy 추가

--- back/src/passport/index.ts
+++ back/src/passport/index.ts
@@ -1,7 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import passport from "passport";
-import google from "./strategies/google";
+import googleOAuth from "./strategies/google";
+import facebookOAuth from "./strategies/facebook";
+import kakaoOAuth from "./strategies/kakao";
 
 export default () => {
-  passport.use("google", google);
-  passport.initialize();
+  passport.use(googleOAuth);
+  passport.use(facebookOAuth);
+  passport.use(kakaoOAuth);
+
+  passport.serializeUser((user: any, done: any) => {
+    done(null, user);
+  });
+
+  passport.deserializeUser((obj: any, done: any) => {
+    done(null, obj);
+  });
 };
