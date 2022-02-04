992f225f9a0bfcf72c3ccdf9ec72c8369da55731
Jisung <jasdepw@gmail.com>
2022-02-04 12:30:01 +0900

    Feat. findOrCreate 추가

--- back/src/passport/strategies/google.ts
+++ back/src/passport/strategies/google.ts
@@ -1,26 +1,31 @@
-import GoogleStrategy from "passport-google-oauth20";
-import { User } from "@src/models/schemas/user.model";
+import { Strategy as GoogleStrategy, StrategyOptions } from "passport-google-oauth20";
+import { findOrCreate } from "../../services/user.service";
+import { passportGoogleConfig } from "../../utils/constants";
 
-const passportConfig = {
-  clientID: "",
-  clientSecret: "",
-  callbackURL: "",
+const passportConfig: StrategyOptions = {
+  clientID: passportGoogleConfig.clientID,
+  clientSecret: passportGoogleConfig.clientSecret,
+  callbackURL: passportGoogleConfig.callbackURL,
 };
 
-export default new GoogleStrategy(passportConfig, async (req, accessToken, refreshToken, profile, done) => {
-  // email, firstName, lastName, nickname, profileImg, source
-  const email = profile.emails[0].value;
-  const firstName = profile.name.givenName;
-  const lastName = profile.name.familyName;
-  const nickname = profile.name;
-  const profileImg = profile.photos[0].value;
-  const source = profile.provider;
+export default new GoogleStrategy(
+  passportConfig,
+  async (req, accessToken, refreshToken, profile, done) => {
+    const email = profile.emails[0].value;
+    const firstName = profile.name.givenName;
+    const lastName = profile.name.familyName;
+    const nickname = profile.name;
+    const profileImg = profile.photos[0].value;
+    const source = profile.provider;
 
-  if (source !== "google") {
-    return done(null, false, {
-      message: "You have previously signed up with a different signin method",
-    });
-  }
+    if (source !== "google") {
+      return done(null, false, {
+        message: "You have previously signed up with a different signin method",
+      });
+    }
 
-  return done;
-});
+    const user = await findOrCreate({ email, firstName, lastName, nickname, profileImg, source });
+
+    return done(null, user);
+  },
+);
--- back/src/services/user.service.ts
+++ back/src/services/user.service.ts
@@ -1,15 +1,34 @@
-import { UserModel } from '@src/models';
+import { UserModel } from "@src/models";
 
-import { IUserDocument, IUserDTO } from '@src/types/User';
+import { IUserDocument, IUserDTO } from "@src/types/User";
 
 export class UserService {
-	constructor(private readonly userModel: typeof UserModel) {}
-	async test() {
-		const user = await this.userModel.findOne({ email: 'asd' });
-		console.log(user);
+  constructor(private readonly userModel: typeof UserModel) {}
+  async test() {
+    const user = await this.userModel.findOne({ email: "asd" });
+    console.log(user);
 
-		return 'test';
-	}
+    return "test";
+  }
 }
+
+export const findOrCreate = async (user: IUserDTO) => {
+  const currentUser = await UserModel.findOne(user.email);
+
+  if (currentUser) {
+    return currentUser;
+  }
+
+  const newUser = await UserModel.create({
+    email: user?.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    nickname: user.nickname,
+    profileImg: user.profileImg,
+    source: user.source,
+  });
+
+  return newUser;
+};
 
 export const userService = new UserService(UserModel);
