18147e4bf697d8b580a0615ac97c465408d08ba0
lth <vusdus91@gmail.com>
2022-02-05 22:15:41 +0900

    Feat. 캠핑장 리뷰 컨테이너 컴포넌트  생성

---
+++ front/src/components/UI/molecules/CampsiteReviewBox/CampsiteReviewBox.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import ReviewCard from "../ReviewCard/ReviewCard";
+
+const StyledContainer = styled.section`
+  width: 1100px;
+`;
+const StyledCountBox = styled.span`
+  display: flex;
+  justify-content: flex-end;
+`;
+const StyledButton = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  transition: color 0.1s;
+  position: relative;
+  :hover:not(.button-active) {
+    color: #f29f05;
+  }
+  &.button-active {
+    font-weight: bold;
+    &::after {
+      content: "";
+      position: absolute;
+      bottom: -2px;
+      right: 0;
+      display: block;
+      left: 0;
+      height: 2px;
+      background-color: #0d0d0d;
+    }
+  }
+`;
+
+const CampsiteReviewBox = () => {
+  return (
+    <StyledContainer>
+      <StyledCountBox>
+        <StyledButton className="button-active">전체(9)</StyledButton>
+        <StyledButton>또 가고 싶어요(4)</StyledButton>
+        <StyledButton>평범해요(3)</StyledButton>
+        <StyledButton>최악입니다(2)</StyledButton>
+      </StyledCountBox>
+      {/* <ReviewCard></ReviewCard>
+      <ReviewCard></ReviewCard>
+      <ReviewCard></ReviewCard>
+      <ReviewCard></ReviewCard> */}
+    </StyledContainer>
+  );
+};
+
+export default CampsiteReviewBox;
