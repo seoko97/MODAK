3813cbd1d5a321b505d908fba47726d965cb5c52
lth <vusdus91@gmail.com>
2022-02-05 15:06:13 +0900

    Feat. 마이페이지 유저 프로필 컴포넌트 생성

---
+++ front/src/components/UI/molecules/MypageProfile/MyPageProfile.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import Title from "../../atoms/Title";
+
+const StyledContainer = styled.figure`
+  display: flex;
+  gap: 100px;
+  padding: 30px;
+`;
+const StyledImageBox = styled.div`
+  width: 150px;
+  height: 150px;
+  @media (max-width: ${({ theme }) => theme.BP.TABLET}) {
+    width: 100px;
+    height: 100px;
+  }
+  @media (max-width: ${({ theme }) => theme.BP.MOBILE}) {
+    display: none;
+  }
+`;
+
+const StyledImage = styled.img`
+  position: absolute;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  @media (max-width: ${({ theme }) => theme.BP.TABLET}) {
+    width: 100px;
+    height: 100px;
+  }
+`;
+const StyledInfo = styled.figcaption`
+  color: gray;
+  & h2,
+  p {
+    margin-bottom: 10px;
+  }
+`;
+
+const MyPageProfile = () => {
+  return (
+    <StyledContainer>
+      <StyledImageBox>
+        <StyledImage src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+      </StyledImageBox>
+      <StyledInfo>
+        <Title size={14}>닉네임</Title>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, possimus. Omnis rerum
+          id totam aut laboriosam consequuntur cum, temporibus vero molestiae reiciendis quos quae
+          doloribus et, commodi quasi esse est!
+        </p>
+        글 13 좋아요 10
+      </StyledInfo>
+    </StyledContainer>
+  );
+};
+
+export default MyPageProfile;
