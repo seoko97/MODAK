19ece6b29e6262b3d59fc6c3f13fd6d7716dd0ac
seokho10007 <sukho1007@naver.com>
2022-02-05 18:33:58 +0900

    Feat. Reducer 유틸 함수 구현

---
+++ front/src/lib/reducerUtils.ts
@@ -0,0 +1,49 @@
+export interface ReducerInit {
+  loading: boolean;
+  done: boolean;
+  error: string | null;
+}
+
+export const asyncPending = (state: ReducerInit) => {
+  state.loading = true;
+  state.done = false;
+  state.error = null;
+};
+
+export const asyncFulfilled = (state: ReducerInit) => {
+  state.loading = false;
+  state.done = true;
+  state.error = null;
+};
+
+export const asyncRejected = (state: ReducerInit, message: string) => {
+  state.loading = true;
+  state.done = false;
+  state.error = message;
+};
+
+export const reducerUtils = {
+  init: () => ({
+    done: false,
+    loading: false,
+    error: null,
+  }),
+
+  loading: () => ({
+    done: false,
+    loading: true,
+    error: null,
+  }),
+
+  success: () => ({
+    done: true,
+    loading: false,
+    error: null,
+  }),
+
+  error: (error: Error) => ({
+    done: false,
+    loading: false,
+    error: error.message,
+  }),
+};
