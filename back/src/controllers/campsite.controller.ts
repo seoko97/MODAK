7b770fcb2ecb4af55697b7084ba0d2abc1c90a47
seokho10007 <sukho1007@naver.com>
2022-02-08 01:41:15 +0900

    Feat. Query Filtering route 생성

--- back/src/controllers/campsite.controller.ts
+++ back/src/controllers/campsite.controller.ts
@@ -1,37 +1,44 @@
-import { Request, Response } from 'express';
-import axios from 'axios';
+import { Request, Response } from "express";
+import axios from "axios";
 
-import { CampsiteService, campsiteService } from '@services/campsite.service';
+import { CampsiteService, campsiteService } from "@services/campsite.service";
 
-import { ICampsiteDocument, ICampsiteDTO } from '@src/types/Campsite';
-import { serviceKey } from '@src/utils/constants';
-import { IAxiosSchduleDTO } from '@src/types';
-import { getCampData } from '@src/utils/dataParser';
+import { ICampQuery, ICampsiteDocument, ICampsiteDTO } from "@src/types/Campsite";
+import { serviceKey } from "@src/utils/constants";
+import { IAxiosSchduleDTO } from "@src/types";
+import { getCampData } from "@src/utils/dataParser";
+import { campsQuery } from "@src/utils/getCampQuery";
 
 export class CampsiteController {
-	constructor(private readonly campsiteService: CampsiteService) {}
-	test = async (req: Request, res: Response) => {
-		const modelTest = await this.campsiteService.test();
+  constructor(private readonly campsiteService: CampsiteService) {}
 
-		const camp = await this.campsiteService.getUserById();
-		console.log(camp);
+  getCamps = async (req: Request, res: Response) => {
+    const query = campsQuery(req.query) as ICampQuery;
 
-		res.send(modelTest);
-	};
+    const camps = await this.campsiteService.getCampsByQuery(query);
 
-	schedule = async () => {
-		try {
-			const camps = await axios.get(serviceKey);
-			await Promise.all(
-				camps.data.response.body.items.item.map((camp: IAxiosSchduleDTO) => {
-					const campsite = getCampData(camp);
-					return this.campsiteService.create(campsite);
-				}),
-			);
-		} catch (e: any) {
-			console.log(e.message);
-		}
-	};
+    res.json(camps);
+  };
+
+  getCamp = async (req: Request, res: Response) => {
+    const camp = await this.campsiteService.getCampById(req.body.id);
+
+    res.json({ status: "ok", data: camp });
+  };
+
+  schedule = async () => {
+    try {
+      const camps = await axios.get(serviceKey);
+      await Promise.all(
+        camps.data.response.body.items.item.map((camp: IAxiosSchduleDTO) => {
+          const campsite = getCampData(camp);
+          return this.campsiteService.create(campsite);
+        }),
+      );
+    } catch (e: any) {
+      console.log(e.message);
+    }
+  };
 }
 
 export const campsiteController = new CampsiteController(campsiteService);
