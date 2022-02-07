27d1140177b016d83a63d1ba597754787b5d755a
lth <vusdus91@gmail.com>
2022-02-07 16:58:09 +0900

    Feat. atoms/SubTitle 생성

---
+++ front/src/components/UI/atoms/SubTitle/index.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+interface Props {
+  size: number;
+}
+
+const StyledSubTitle = styled.h2<Props>`
+  font-size: ${({ size }) => size}px;
+  color: #757575;
+`;
+const SubTitle: FC<Props> = ({ size, children }) => (
+  <StyledSubTitle size={size}>{children}</StyledSubTitle>
+);
+
+SubTitle.defaultProps = {
+  size: 13,
+};
+
+export default SubTitle;
