4e7a05104ac417e774f1be9b3b98146e913d8f32
seokho10007 <sukho1007@naver.com>
2022-02-08 01:39:47 +0900

    Feat. DocumentArray Type 설정

--- back/src/types/User.ts
+++ back/src/types/User.ts
@@ -1,29 +1,29 @@
-import { Document, Model, PopulatedDoc } from 'mongoose';
-import { ICampsiteDocument } from './Campsite';
-import { IReviewDocument } from './Review';
+import { Document, Model, Types } from "mongoose";
+import { ICampsiteDocument } from "./Campsite";
+import { IReviewDocument } from "./Review";
 
 export interface ITokenUser {
-	id: string;
+  id: string;
 }
 
 export interface IUserDTO {
-	email: string;
-	firstName: string;
-	lastName: string;
-	nickname: string;
-	profileImg: string | null;
-	source: 'Google' | 'Kakao' | 'Facebook';
+  email: string;
+  firstName: string;
+  lastName: string;
+  nickname: string;
+  profileImg: string | null;
+  source: "Google" | "Kakao" | "Facebook";
 }
 
 export interface IUserDocument extends IUserDTO, Document {
-	refreshToken: string | null;
-	reviews: PopulatedDoc<IReviewDocument>[];
-	bookmark: PopulatedDoc<ICampsiteDocument>[];
+  refreshToken: string | null;
+  reviews: Types.DocumentArray<IReviewDocument>[];
+  bookmark: Types.DocumentArray<ICampsiteDocument>[];
 
-	comparePassword(aPassword: string): Promise<boolean>;
-	verifyRefresh(): boolean;
+  comparePassword(aPassword: string): Promise<boolean>;
+  verifyRefresh(): boolean;
 }
 
 export interface IUserModel extends Model<IUserDocument> {
-	hashPassword(userData: IUserDTO): Promise<void>;
+  hashPassword(userData: IUserDTO): Promise<void>;
 }
