5a0355d86f86614f7aef159dad22a604cc1ad637
seokho10007 <sukho1007@naver.com>
2022-02-10 22:27:08 +0900

    Feat. Review CRUD 작성

--- back/src/controllers/review.controller.ts
+++ back/src/controllers/review.controller.ts
@@ -1,7 +1,10 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import fs from "fs";
+import { ITokenUser } from "~types/User";
+import { IReviewDocument, IReviewDTO } from "~types/Review";
 import { ReviewService, reviewService } from "@services/review.service";
-import { IReviewDocument } from "@src/types/Review";
+import { userService, UserService } from "@services/user.service";
+import resizeImage from "@utils/resizeImage";
 
 try {
   fs.accessSync("uploads");
@@ -10,23 +13,69 @@ try {
 }
 
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
-  test = async (req: Request, res: Response) => {
-    const modelTest = await this.reviewService.test();
+  constructor(
+    private readonly reviewService: ReviewService,
+    private readonly userService: UserService,
+  ) {}
 
-    res.send(modelTest);
+  getReviews: RequestHandler = async (req, res) => {
+    const { location } = req.body;
+    const { lastId } = req.query;
+
+    const reviews = await this.reviewService.getReviewsByQuery({ _id: { $gt: lastId }, location });
+
+    res.json({ status: true, reviews });
   };
 
-  create = async (req: Request, res: Response) => {
-    const { content, author, locations, shopname } = req.body;
-    const photes = req.files;
+  create: RequestHandler = async (req, res) => {
+    const _user = req.user as ITokenUser;
+    const { content, location, rating, photos } = req.body as Omit<IReviewDTO, "author">;
 
-    res.send("asd");
+    const review = await this.reviewService.create({
+      content,
+      location,
+      rating,
+      photos,
+      author: _user,
+    });
+
+    res.send({ status: true, review });
+  };
+  update: RequestHandler = async (req, res, next) => {
+    const { id } = req.params;
+    const { content, location, rating, photos } = req.body as Omit<IReviewDTO, "author">;
+
+    const review = await this.reviewService.update(id, {
+      content,
+      location,
+      rating,
+      photos,
+    });
+
+    if (!review) return next({ message: "리뷰가 존재하지 않습니다." });
+
+    res.send({ status: true, review });
   };
 
-  getReview = async (req: Request, res: Response) => {
-    res.send("asd");
+  delete: RequestHandler = async (req, res) => {
+    const { id } = req.params;
+
+    await this.reviewService.delete(id);
+
+    res.json({ status: true });
+  };
+
+  uploadImage: RequestHandler = async (req, res, next: NextFunction) => {
+    const photos = req.files as Express.Multer.File[];
+
+    if (!photos) return next({ message: "이미지가 존재하지 않습니다." });
+
+    await resizeImage(photos);
+
+    const images = photos.map((photo) => photo.filename);
+
+    res.json({ status: true, images });
   };
 }
 
-export const reviewController = new ReviewController(reviewService);
+export const reviewController = new ReviewController(reviewService, userService);
