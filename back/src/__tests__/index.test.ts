ac3c47d90a6c3d33414212a5794c0dc1bf83cd17
seokho10007 <sukho1007@naver.com>
2022-02-11 21:37:29 +0900

    Feat. Jest DB 연결

--- back/src/__tests__/index.test.ts
+++ back/src/__tests__/index.test.ts
@@ -1,10 +1,27 @@
-import app from '@src/app';
-import request from 'supertest';
+import app from "@src/app";
+import { configs } from "@utils/constants";
+import mongoose from "mongoose";
+import request from "supertest";
 
-describe('GET /', () => {
-	it('returns status 304 Main /', async () => {
-		const res = await request(app).get('/');
+describe("GET /", () => {
+  beforeAll(async () => {
+    await mongoose.connect(configs.DB_URL).then(() => console.log("성공"));
+  });
 
-		expect(res.statusCode).toEqual(200);
-	});
+  it("returns status 200 Main /", async () => {
+    const res = await request(app).get("/").send();
+
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it("유저 정보 조회", async () => {
+    const res = await request(app).get("/api/user/6205bad7f13438a35b7804ff").send();
+
+    expect(res.statusCode).toEqual(201);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+    await mongoose.disconnect();
+  });
 });
