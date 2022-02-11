21b81f6281db8b99396de9bb1459b2dbc0586f0c
seokho10007 <sukho1007@naver.com>
2022-02-12 02:15:34 +0900

    Chore. Modal 예시 코드

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { NextPage } from "next";
+import UserChange from "@src/components/modals/UserUpdateForm";
+import styled from "styled-components";
+import useModal from "@src/hooks/useInput";
+
+const Container = styled.div`
+  height: 3000px;
+`;
 
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  const [isOpen, onOpen, onClose] = useModal();
+
+  return (
+    <div>
+      <button onClick={onOpen}>click</button>
+      {isOpen && <UserChange onClick={onClose} />}
+      <Container />
+    </div>
+  );
 };
 
 export default Home;
