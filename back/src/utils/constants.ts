bdc2c6c8250f458b8ef4f1529a0bf770b3575acf
seokho10007 <sukho1007@naver.com>
2022-02-11 22:50:34 +0900

    Feat.DB Connect 함수 모듈화

--- back/src/utils/constants.ts
+++ back/src/utils/constants.ts
@@ -16,12 +16,14 @@ export const prod = process.env.NODE_ENV === "production";
 
 export const url = prod ? process.env.PRUDUCT_URL : "http://localhost:3060";
 
-export const APIURL = `http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/basedList?APIURL=${process.env.SERVICE_KEY}&pageNo=1&numOfRows=3000&MobileOS=ETC&MobileApp=AppTest&_type=json`;
+export const APIURL = `http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/basedList?serviceKey=${process.env.SERVICE_KEY}&pageNo=1&numOfRows=3000&MobileOS=ETC&MobileApp=AppTest&_type=json`;
 
 export const configs = {
   DB_ID: process.env.DB_ID as string,
   DB_PASSWORD: process.env.DB_PASSWORD as string,
+  DB_URL: process.env.DB_URL as string,
   PORT: (process.env.PORT || "3065") as string,
+  TEST: process.env.NODE_ENV === "test",
 };
 
 export const passportGoogleConfig = {
