fd176a9b3838ef4a382ebcbcaecb225204c24a78
seokho10007 <sukho1007@naver.com>
2022-02-12 23:47:48 +0900

    Feat. User Async Action 생성

--- front/src/reducers/user/action.ts
+++ front/src/reducers/user/action.ts
@@ -1,7 +1,9 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { userAPI } from "@src/apis";
+import { createActionByProps, createAction } from "@src/lib/createActionHandler";
 
-export const logIn = createAsyncThunk("user/logIn", async () => {
-  return new Promise<string>((r) => {
-    setTimeout(() => r("asd"), 1000);
-  });
-});
+const signout = createAction("user/signout", userAPI.signout);
+const getSigninUser = createAction("user/signinUser", userAPI.getSinginUser);
+const getUserInfo = createActionByProps("user/pageUser", userAPI.getUserInfo);
+const editUserInfo = createActionByProps("user/edit", userAPI.editUserInfo);
+
+export { signout, getSigninUser, getUserInfo, editUserInfo };
