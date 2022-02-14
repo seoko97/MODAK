08c0fafe4dfb72461d6711f9f5890a1e1f915f22
seokho10007 <sukho1007@naver.com>
2022-02-15 02:03:50 +0900

    Chore. Icon 추가

---
+++ front/src/components/icons/ExitIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ExitIcon = () => (
+  <svg viewBox="0 0 32 32">
+    <path
+      d="M25 7L7 25"
+      stroke="black"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M25 25L7 7"
+      stroke="black"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default ExitIcon;
