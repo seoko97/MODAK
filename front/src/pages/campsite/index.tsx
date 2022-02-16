7a008a10ad37721e2079f6b808a013bb54e160b2
seokho10007 <sukho1007@naver.com>
2022-02-17 00:46:44 +0900

    Feat. 쿼리문 변경

--- front/src/pages/campsite/index.tsx
+++ front/src/pages/campsite/index.tsx
@@ -5,6 +5,10 @@ import FilterFinder from "@src/components/UI/molecules/FilterFinder";
 import CampSiteListBox from "@src/components/UI/molecules/CampsiteListBox";
 import SortButton from "@src/components/UI/molecules/SortButton";
 import RowFrame from "@src/components/UI/templates/RowFrame";
+import axios, { HeadersDefaults } from "axios";
+import wrapper from "@src/store/configureStore";
+import { getSigninUser } from "@src/reducers/user/action";
+import { getUserReviews } from "@src/reducers/reviews/action";
 
 const CampsiteListPage: NextPage = () => {
   return (
@@ -22,3 +26,24 @@ const CampsiteListPage: NextPage = () => {
 };
 
 export default CampsiteListPage;
+
+interface RequestHeader extends HeadersDefaults {
+  Cookie: string;
+}
+
+export const getServerSideProps = wrapper.getServerSideProps((store) => async (ctx) => {
+  const cookies = ctx.req?.headers?.cookie;
+
+  if (cookies) (axios.defaults.headers as RequestHeader).Cookie = cookies;
+
+  const signUserRes = await store.dispatch(getSigninUser());
+
+  const setCookies = (signUserRes.payload as any)?.headers?.["set-cookie"];
+  if (setCookies) ctx.res.setHeader("Set-Cookie", setCookies);
+
+  await store.dispatch(getUserReviews({ userId: "6205bad7f13438a35b7804ff" }));
+
+  return {
+    props: {},
+  };
+});
