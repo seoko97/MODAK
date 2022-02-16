b3b009d312021a48022277ecef66ef9d3d21e383
Yoonseong Kim <rladbstjd96@naver.com>
2022-02-16 17:56:24 +0900

    Feat. 캠핑장 리뷰 페이지

--- front/src/pages/camp/[id].tsx
+++ front/src/pages/camp/[id].tsx
@@ -4,6 +4,11 @@ import RowFrame from "@templates/RowFrame";
 import CampSiteCoverImg from "@molecules/CampSiteCoverImg";
 import CampSiteInfo from "@molecules/CampSiteInfo";
 import CampsiteReviewBox from "@organisms/CampsiteReviewBox";
+import wrapper from "@src/store/configureStore";
+import { getCamp } from "@src/reducers/camp/action";
+import axios, { HeadersDefaults } from "axios";
+import { getSigninUser } from "@src/reducers/user/action";
+import { getCampReviews } from "@src/reducers/reviews/action";
 
 const Camp: NextPage = () => {
   return (
@@ -18,3 +23,25 @@ const Camp: NextPage = () => {
 };
 
 export default Camp;
+
+interface RequestHeader extends HeadersDefaults {
+  Cookie: string;
+}
+
+export const getServerSideProps = wrapper.getServerSideProps((store) => async (ctx) => {
+  const cookies = ctx.req?.headers?.cookie;
+  const query = ctx?.query;
+  if (cookies) (axios.defaults.headers as RequestHeader).Cookie = cookies;
+
+  const signUserRes = await store.dispatch(getSigninUser());
+
+  const setCookies = (signUserRes.payload as any)?.headers?.["set-cookie"];
+  if (setCookies) ctx.res.setHeader("Set-Cookie", setCookies);
+
+  await store.dispatch(getCamp(query.id as string));
+  await store.dispatch(getCampReviews({ campId: query.id as string }));
+
+  return {
+    props: {},
+  };
+});
