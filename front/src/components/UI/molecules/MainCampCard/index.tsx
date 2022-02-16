5ccc5c97da1ab2d1c3c5ec4815710167da1648c6
seokho10007 <sukho1007@naver.com>
2022-02-16 14:45:05 +0900

    Feat. Slider 생성

---
+++ front/src/components/UI/molecules/MainCampCard/index.tsx
@@ -0,0 +1,147 @@
+import React from "react";
+import styled from "styled-components";
+import Link from "@atoms/Link";
+
+import PinIcon from "@src/components/icons/Pin";
+import BookmarkIcon from "@src/components/icons/BookmarkIcon";
+import CommentIcon from "@src/components/icons/CommentIcon";
+import { ICamp } from "@src/types/reducers/camp";
+
+interface Props {
+  camp: ICamp;
+  url: string;
+}
+
+const Container = styled.div`
+  width: 95%;
+  cursor: pointer;
+  margin: 0 auto;
+  border-radius: 8px;
+  box-shadow: 1px 2px 6px 0px rgb(158 154 153 / 32%);
+  & * {
+    color: ${({ theme }) => theme.FONT_COLOR.PRIMARY_COLOR};
+  }
+`;
+
+const ImageWrapper = styled.div`
+  width: 100%;
+
+  & > div {
+    position: relative;
+    width: 100%;
+    padding-bottom: 200px;
+  }
+
+  & img {
+    border-radius: 8px 8px 0 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  @media (max-width: ${({ theme }) => theme.BP.PC}) {
+    & > div {
+      padding-bottom: 70%;
+      max-height: 320px;
+    }
+  }
+`;
+
+const ContentWrapper = styled.div`
+  width: 100%;
+  padding: 14px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 0 0 8px 8px;
+`;
+
+const ContentHeader = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 3px;
+  gap: 8px;
+  & > h1 {
+    font-size: 18px;
+    overflow: hidden;
+  }
+`;
+
+const LocationWrapper = styled.div`
+  display: flex;
+  font-size: 12px;
+  line-height: 12px;
+  gap: 5px;
+  margin-bottom: 5px;
+  & > h2 {
+    color: #757575;
+  }
+`;
+
+const ThemaWrapper = styled.div`
+  width: 100%;
+  padding: 12px 0;
+  display: flex;
+  gap: 10px;
+  min-height: 60px;
+  font-size: 12px;
+`;
+
+const ContentFooter = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 10px;
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const MainCampCard = ({ camp, url }: Props) => {
+  const { name, address, thema, category, totalBookmark, views } = camp;
+
+  return (
+    <Link href={url}>
+      <Container>
+        <ImageWrapper>
+          <div>
+            <img src={"/tent.jpg"} alt="img" />
+          </div>
+        </ImageWrapper>
+        <ContentWrapper>
+          <ContentHeader>
+            <h1>{name}</h1>
+            <LocationWrapper>
+              <PinIcon size={16} />
+              <h2>{address}</h2>
+            </LocationWrapper>
+          </ContentHeader>
+          <ThemaWrapper>
+            {thema.map((el) => (
+              <span key={el}>#{el}</span>
+            ))}
+            {category.map((el) => (
+              <span key={el}>#{el}</span>
+            ))}
+          </ThemaWrapper>
+          <ContentFooter>
+            <IconWrapper>
+              <BookmarkIcon size={16} />
+              <span>{totalBookmark || 0}</span>
+            </IconWrapper>
+            <IconWrapper>
+              <CommentIcon size={16} />
+              <span>{views || 0}</span>
+            </IconWrapper>
+          </ContentFooter>
+        </ContentWrapper>
+      </Container>
+    </Link>
+  );
+};
+
+export default MainCampCard;
---
+++ front/src/components/icons/ArrowIcon/Next.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Next = (props: any) => (
+  <svg viewBox="0 0 7 10" {...props}>
+    <path d="M6.85156 8.82812L5.67969 10L0.679688 5L5.67969 0L6.85156 1.17188L3.02344 5L6.85156 8.82812Z" />
+  </svg>
+);
+export default Next;
