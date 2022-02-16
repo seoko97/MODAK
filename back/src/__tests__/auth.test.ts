0a69af99359f01c70c1b027049c5d4e3585b2064
Jisung <jasdepw@gmail.com>
2022-02-16 18:12:44 +0900

    Fix. 잘못 푸시했던 파일 수정

--- back/src/__tests__/auth.test.ts
+++ back/src/__tests__/auth.test.ts
@@ -1,31 +1,22 @@
-import mongoose from "mongoose";
 import request from "supertest";
 import app from "@src/app";
-import { configs } from "@utils/constants";
-import { user } from "./test.config";
 
-describe("GET /", () => {
-  beforeAll(async () => {
-    await mongoose.connect(configs.DB_URL).then(() => console.log("데이터베이스 연결 성공"));
+describe("OAuth login test", () => {
+  test("Google OAuth login page", async () => {
+    const res = await request(app).get("/api/auth/google").send();
+
+    expect(res.statusCode).toEqual(302);
+    expect(res.header.location).toEqual(
+      expect.stringContaining("https://accounts.google.com/o/oauth2/v2/auth"),
+    );
   });
 
-  test("1. 메인페이지 응답", async () => {
-    const res = await request(app).get("/").send();
+  test("Kakao OAuth login page", async () => {
+    const res = await request(app).get("/api/auth/kakao").send();
 
-    expect(res.statusCode).toEqual(200);
-  });
-
-  test("2. 유저 정보 조회", async () => {
-    const res = await request(app)
-      .get("/api/user/" + user)
-      .send();
-
-    expect(res.statusCode).toEqual(200);
-    expect(res.text).toContain(user);
-  });
-
-  afterAll(async () => {
-    await mongoose.connection.close();
-    await mongoose.disconnect();
+    expect(res.statusCode).toEqual(302);
+    expect(res.header.location).toEqual(
+      expect.stringContaining("https://kauth.kakao.com/oauth/authorize"),
+    );
   });
 });
