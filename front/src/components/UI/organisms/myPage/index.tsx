4dce26a61ac9f13b11438be6ac8493dd24e5dc83
hamelln <vusdus91@gmail.com>
2022-02-18 13:50:37 +0900

    Style. 마이페이지 수정

--- front/src/components/UI/organisms/myPage/index.tsx
+++ front/src/components/UI/organisms/myPage/index.tsx
@@ -1,37 +1,32 @@
 import React, { memo, useCallback, useEffect, useMemo, useState } from "react";
 import { useDispatch } from "react-redux";
-import { AppDispatch, useAppSelector } from "@store/configureStore";
+import { useAppSelector } from "@src/store/configureStore";
 import { getUserReviews as getReview } from "@reducers/reviews/action";
 import { getUserCamps as getCamps } from "@reducers/camps/action";
-import RowFrame from "@templates/RowFrame";
-import MyPageProfile from "@src/components/UI/organisms/MypageProfile";
-import Tabs from "../Tabs";
-import MyReview from "./MyReview";
-import WishList from "./WishList";
-import Style from "./styles";
+import WishList from "@src/components/UI/organisms/WishList";
+import MyPageProfile from "../MyPageProfile";
+import Tabs from "../../molecules/MyProfileTabs";
+import Style from "./style";
+import RowFrame from "../../templates/RowFrame";
+import MyReview from "../MyReview";
 
 interface Props {
   userId: string;
 }
 
 const MyPage = ({ userId }: Props) => {
-  const dispatch: AppDispatch = useDispatch();
   const [tab, setTab] = useState<string>("내 리뷰");
   const { mainCamps, getUserCamps } = useAppSelector((state) => state.camps);
   const { loading: campLoading, error: campError } = getUserCamps;
   const { mainReviews, getUserReviews } = useAppSelector((state) => state.reviews);
   const { loading: reviewLoading, error: reviewError } = getUserReviews;
-  useEffect(() => {
-    console.log(tab);
-  }, [tab]);
+  const dispatch = useDispatch();
 
   const getComponentByTab = useMemo(() => {
     switch (tab) {
       case "내 리뷰":
-        // ? 유저 리뷰 리스트 : organism
         return mainReviews.map((review) => <MyReview key={review._id} review={review} />);
       case "찜한 캠핑장":
-        // ? 위시 리스트 : organism
         return mainCamps.map((camp) => <WishList key={camp._id} camp={camp} />);
       default:
         return null;
@@ -41,16 +36,13 @@ const MyPage = ({ userId }: Props) => {
   const handleClick = useCallback(async (e) => {
     const text = e.target.innerText;
     if (text === "내 리뷰") await dispatch(getReview({ userId }));
-    // else await dispatch(getCamps({ userId }));
     setTab(e.target.innerText);
   }, []);
 
   return (
     <RowFrame>
       <Style.Main>
-        {/* 마이페이지 프로필: organism */}
         <MyPageProfile />
-        {/* 탭스 : molucules */}
         <Tabs current={tab} onClick={handleClick}></Tabs>
         {getComponentByTab}
       </Style.Main>
