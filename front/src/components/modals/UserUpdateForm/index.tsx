ca2ffe0fa1018e525ad94bf2627c610fa57cc269
hamelln <vusdus91@gmail.com>
2022-02-18 13:41:47 +0900

    Feat. 모달창 유저 정보 수정 기능

--- front/src/components/modals/UserUpdateForm/index.tsx
+++ front/src/components/modals/UserUpdateForm/index.tsx
@@ -1,11 +1,14 @@
-import React, { SetStateAction, useRef, Dispatch } from "react";
-import styled, { css } from "styled-components";
-import ModalLayout from "@modals/ModalLayout";
-import TrashCanIcon from "@icons/TrashCanIcon";
-import ExitIcon from "@icons/ExitIcon";
-import { useAppSelector } from "@store/configureStore";
-import { EditUserData } from "@type/apis/user";
-import { IUser } from "@type/reducers/user";
+import React, { SetStateAction, useRef, Dispatch, useEffect, useState, useCallback } from "react";
+import ModalLayout from "@src/components/modals/ModalLayout";
+import TrashCanIcon from "@src/components/icons/TrashCanIcon";
+import ExitIcon from "@src/components/icons/ExitIcon";
+import { useAppSelector } from "@src/store/configureStore";
+import { EditUserData } from "@src/types/apis/user";
+import { useDispatch } from "react-redux";
+import { editUserInfo, getUserInfo, uploadProfileImg } from "@src/reducers/user/action";
+import { url } from "@src/apis";
+import { checkUrl } from "@src/lib/checkUrl";
+import Style from "./style";
 
 interface Props {
   onClick: () => void;
@@ -14,264 +17,94 @@ interface Props {
 const UserUpdate = ({ onClick }: Props) => {
   const { me } = useAppSelector((state) => state.user);
   const { nickname, intro, profileImg } = me as EditUserData;
-  const imageRef = React.useRef<HTMLInputElement | null>(null);
-  const nameRef = useRef<HTMLInputElement | null>(null);
-  const introRef = useRef<HTMLTextAreaElement | null>(null);
+  const [name, setName] = useState(nickname);
+  const [desc, setDesc] = useState(intro);
+  const [img, setImg] = useState(profileImg);
+  const imageRef = useRef<HTMLInputElement | null>(null);
+  const dispatch = useDispatch();
 
-  const onChangeClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
+  const nameHandler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  }, []);
+
+  const descHandler = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDesc(e.target.value);
+  }, []);
+
+  const pullImageHandler = useCallback(() => {
     imageRef.current?.click();
+  }, [imageRef.current]);
+
+  const imageHandler = useCallback(async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files === null) return;
+
+    const file = e.target.files[0];
+
+    const data = new FormData();
+    data.append("img", file);
+
+    const res: any = await dispatch(uploadProfileImg(data));
+
+    setImg(res.payload.image);
+  }, []);
+
+  const deleteImage = () => {
+    setImg(
+      "https://images.unsplash.com/photo-1594495894542-a46cc73e081a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+    );
   };
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    if (e.currentTarget === null) {
-      return;
-    }
-    const updated: IUser = { ...me, [e.currentTarget.name]: e.currentTarget.value };
-    // updateUser(updated);
-  };
-
-  // ! 이미지 url을 받아와서 바꿔야함.
-  const onChangeImage = async () => {
-    // updateUser({
-    //   ...me,
-    //   profileImg:
-    //     "https://images.unsplash.com/photo-1579783483458-83d02161294e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1397&q=80",
-    // });
-  };
-
-  const deleteImage = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    // updateUser({
-    //   ...me,
-    //   profile: "",
-    // });
-  };
-
-  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const _nickname = e.target.nickname.value.trim();
-    if (_nickname === "") {
+  const handleSubmit = async () => {
+    if (name === "") {
       alert("닉네임은 한 글자 이상 입력해주세요.");
       return;
     }
-    const _intro = e.target.intro.value;
-    // const updated = { ...me, nickname: _nickname, intro: _intro };
-    // updateUser(updated);
-    onClick();
-  };
 
-  const exitUser = () => {
-    confirm("탈퇴하시겠어요?");
+    await dispatch(editUserInfo({ nickname: name, profileImg: img, intro: desc }));
+    onClick();
   };
 
   return (
     <ModalLayout onClick={onClick}>
-      <Container onSubmit={handleSubmit}>
-        <ExitModal onClick={onClick}>
+      <Style.Container>
+        <Style.ExitModal onClick={onClick}>
           <ExitIcon />
-        </ExitModal>
-        <Header>
-          <HeaderTitle>회원정보수정</HeaderTitle>
-        </Header>
-        <EditContainer>
-          <EditTitle>프로필 이미지</EditTitle>
-          <EditImage>
-            <ImageInput
+        </Style.ExitModal>
+        <Style.Header>
+          <Style.HeaderTitle>회원정보수정</Style.HeaderTitle>
+        </Style.Header>
+        <Style.EditContainer>
+          <Style.EditTitle>프로필 이미지</Style.EditTitle>
+          <Style.EditImage>
+            <Style.ImageInput
               ref={imageRef}
               type="file"
-              accept="image/*"
+              accept=".jpg .jpeg .png"
               name="file"
-              onChange={onChangeImage}
+              onChange={imageHandler}
             />
-            <ImageContainer onClick={onChangeClick}>
-              <ProfileImage src={profileImg} alt="profile__image"></ProfileImage>
-            </ImageContainer>
-            <DeleteImage onClick={deleteImage}>
+            <Style.ImageContainer onClick={pullImageHandler}>
+              <Style.ProfileImage src={checkUrl(img)} alt="profile__image"></Style.ProfileImage>
+            </Style.ImageContainer>
+            <Style.DeleteImage onClick={deleteImage}>
               <TrashCanIcon />
-            </DeleteImage>
-          </EditImage>
-        </EditContainer>
-        <EditContainer>
-          <EditTitle>닉네임</EditTitle>
-          <InputName
-            type="text"
-            name="nickname"
-            ref={nameRef}
-            value={nickname}
-            onChange={onChange}
-          />
-        </EditContainer>
-        <EditContainer>
-          <EditTitle>소개</EditTitle>
-          <InputText name="intro" ref={introRef} value={intro} onChange={onChange}></InputText>
-        </EditContainer>
-        <EditContainer>
-          <ModifyButton>회원 정보 수정</ModifyButton>
-        </EditContainer>
-        <UserExit onClick={exitUser}>탈퇴하기</UserExit>
-      </Container>
+            </Style.DeleteImage>
+          </Style.EditImage>
+        </Style.EditContainer>
+        <Style.EditContainer>
+          <Style.EditTitle>닉네임</Style.EditTitle>
+          <Style.InputName type="text" name="nickname" value={name} onChange={nameHandler} />
+        </Style.EditContainer>
+        <Style.EditContainer>
+          <Style.EditTitle>소개</Style.EditTitle>
+          <Style.InputText name="intro" value={desc} onChange={descHandler}></Style.InputText>
+        </Style.EditContainer>
+        <Style.EditContainer>
+          <Style.ModifyButton onClick={handleSubmit}>회원 정보 수정</Style.ModifyButton>
+        </Style.EditContainer>
+      </Style.Container>
     </ModalLayout>
   );
 };
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  width: 500px;
-  padding: 50px;
-  z-index: 1002;
-  box-sizing: border-box;
-  background-color: #f7f7f7;
-  color: #0c0c0c;
-  border-radius: 10px;
-  & > button {
-    align-self: flex-end;
-  }
-  @media (max-width: ${({ theme }) => theme.BP.MOBILE}) {
-    width: 100%;
-    & button {
-      align-self: center;
-    }
-  }
-`;
-
-const Header = styled.div`
-  display: flex;
-  width: 100%;
-  margin-bottom: 10px;
-`;
-
-const HeaderTitle = styled.div`
-  flex: 1 0 0px;
-  font-size: 24px;
-  font-weight: bold;
-`;
-
-const UserExit = styled.button`
-  border: none;
-  background-color: transparent;
-  text-decoration: underline;
-  cursor: pointer;
-  position: absolute;
-  bottom: 20px;
-  right: 20px;
-`;
-
-const EditContainer = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  margin: 12px auto;
-  @media (max-width: ${({ theme }) => theme.BP.MOBILE}) {
-    flex-direction: column;
-  }
-`;
-
-const EditTitle = styled.div`
-  width: 100px;
-  @media (max-width: ${({ theme }) => theme.BP.MOBILE}) {
-    width: 200px;
-    color: #494949;
-  }
-`;
-
-const Input = css`
-  :focus {
-    outline: none;
-    box-shadow: 0 0 0 2px #f2b705;
-  }
-  transition: all 0.2s;
-  box-sizing: border-box;
-  padding: 0 15px;
-  border: 1px solid #dbdbdb;
-  border-radius: 2px;
-  height: 36px;
-  color: #1b1b1b;
-  width: 200px;
-  position: relative;
-`;
-const InputName = styled.input`
-  ${Input};
-`;
-
-const InputText = styled.textarea`
-  ${Input};
-  height: 54px;
-`;
-
-const EditImage = styled.div`
-  width: 200px;
-  height: 200px;
-  position: relative;
-  transition: all 0.1s;
-`;
-
-const ImageContainer = styled.div`
-  cursor: pointer;
-  width: 200px;
-  height: 200px;
-  transition: opacity 0.1s;
-  position: relative;
-  :hover {
-    opacity: 0.5;
-  }
-`;
-
-const ImageInput = styled.input`
-  display: none;
-`;
-
-const ProfileImage = styled.img`
-  width: 200px;
-  height: 200px;
-`;
-
-const DeleteImage = styled.button`
-  position: absolute;
-  right: 6px;
-  top: 6px;
-  background-color: #038c5a;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 24px;
-  height: 24px;
-  border: none;
-  border-radius: 2px;
-  & svg {
-    fill: #f3f3f3;
-  }
-  :hover {
-    background-color: #025939;
-  }
-  transition: all 0.1s;
-`;
-
-const ModifyButton = styled.button`
-  width: 200px;
-  height: 50px;
-  background-color: #f29f05;
-  border-radius: 2px;
-  border: none;
-  color: #fff;
-  cursor: pointer;
-  margin: auto;
-`;
-
-const ExitModal = styled.button`
-  position: absolute;
-  top: 12px;
-  right: 12px;
-  background-color: transparent;
-  cursor: pointer;
-  border: none;
-  width: 18px;
-  height: 18px;
-  padding: 0;
-`;
 
 export default UserUpdate;
