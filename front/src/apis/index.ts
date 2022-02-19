1b5d3f3a11b66eea2a42ba45c7c406338500b85d
seokho10007 <sukho1007@naver.com>
2022-02-19 19:44:49 +0900

    주소변경

--- front/src/apis/index.ts
+++ front/src/apis/index.ts
@@ -4,7 +4,7 @@ import campAPI from "./camp";
 import reviewAPI from "./review";
 
 export const url =
-  process.env.NODE_ENV === "production" ? process.env.BASE_URL : "http://localhost:3065";
+  process.env.NODE_ENV === "production" ? process.env.PRODUCTION_URL : "http://localhost:3065";
 
 axios.defaults.baseURL = `${url}/api/`;
 axios.defaults.withCredentials = true;
